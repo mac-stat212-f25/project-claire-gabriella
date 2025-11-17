9bbc07e14b80707bf4591e6b7119586a14444272
Claire Kuno <ckuno@macalester.edu>
2025-11-17 12:38:01 -0600

    imessage things

---
+++ data/raw/exporting.R
@@ -0,0 +1,34 @@
+# Load required packages
+library(RSQLite)
+library(dplyr)
+library(readr)
+
+# Connect to the database (using a copy if needed)
+db <- dbConnect(SQLite(), "~/Documents/chat_copy.db")
+
+query <- "
+SELECT
+    m.rowid,
+    datetime(m.date / 1000000000 + strftime('%s','2001-01-01'), 'unixepoch') AS date,
+    h.id AS sender,
+    m.text
+FROM message m
+LEFT JOIN handle h ON m.handle_id = h.rowid
+WHERE h.id = '+19177941080' AND m.text IS NOT NULL
+ORDER BY m.date
+;"
+
+
+# Run the query and get the messages
+msg_df <- dbGetQuery(db, query)
+msg_df <- msg_df %>%
+  filter(!is.na(text))   # remove NA text
+
+
+# Save to CSV
+write_csv(msg_df, "~/Desktop/imessage_export.csv")
+
+# Disconnect from database
+dbDisconnect(db)
+
+saveRDS(msg_df, "msg_df.rds")
